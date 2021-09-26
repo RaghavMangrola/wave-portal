2e27462f5a6403264129eec332c5711cc28c7564
RaghavMangrola <dev@raghav.io>
2021-09-26 14:32:57 -0700

    Store wave data on smart contract

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,25 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint256 totalWaves;
+
     constructor() {
-        console.log("Smart Contract");
+        console.log("I'm a little Smart Contract, short and stout!");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    // TODO: Figure out data types I can store on the blockchain
+    function submitEmoji() public {
+
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
